f2897a6551b04869d06fc5ba3b412f93a30e7f2f
Friff14 <friffsockpuppet@gmail.com>
2016-11-14 10:07:10 -0700

    Updated all routes.

--- data/route.py
+++ data/route.py
@@ -1,6 +1,7 @@
 import falcon
 import json
-from data import instructorcontroller, semestercontroller, sectioncontroller, roomcontroller
+from data import instructorcontroller, semestercontroller, sectioncontroller, roomcontroller, prefixcontroller
+from data import buildingcontroller, campuscontroller, coursecontroller, featurecontroller
 from wsgiref import simple_server
 
 app = falcon.API()
@@ -9,11 +10,22 @@ instructor = instructorcontroller.InstructorController()
 semester = semestercontroller.SemesterController()
 section = sectioncontroller.SectionController()
 room = roomcontroller.RoomController()
+prefix = prefixcontroller.PrefixController()
+building = buildingcontroller.BuildingController()
+campus = campuscontroller.CampusController()
+course = coursecontroller.CourseController()
+feature = featurecontroller.FeatureController()
 
 app.add_route('/instructor/{instructor_id}', instructor)
 app.add_route('/semester/{semester_id}', semester)
 app.add_route('/section/{section_id}', section)
 app.add_route('/room/{room_id}', room)
+app.add_route('/prefix/{prefix_id}', prefix)
+app.add_route('/building/{building_id}', building)
+app.add_route('/campus/{campus_id}', campus)
+app.add_route('/course/{course_id}', course)
+app.add_route('/feature/{feature_id}', feature)
+
 
 if __name__ == '__main__':
     httpd = simple_server.make_server('localhost', 8000, app)
