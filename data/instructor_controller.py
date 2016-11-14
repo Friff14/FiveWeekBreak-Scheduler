8294feef74f1ec7f5d37680a61d9b61418b03f5a
Friff14 <friffsockpuppet@gmail.com>
2016-11-14 09:04:02 -0700

    Instructor controller now has on_get example.

--- data/instructor_controller.py
+++ data/instructor_controller.py
@@ -1,3 +1,7 @@
+import json
+
+import falcon
+
 from data.tables import *
 
 DBSession = sessionmaker(bind=engine)
@@ -6,34 +10,37 @@ session = DBSession(autocommit=True)
 session.begin()
 
 
-def put(data):
-    instructor = session.query(Instructor).filter(instructor_id=data['instructor_id']).first()
-    instructor.instructor_name = data['instructor_name']
-    instructor.instructor_hours_required = data['instructor_hours_required']
-    instructor.instructor_notes = data['instructor_notes']
-    # ...and so on
+class instructor_controller(object):
+    def put(self, data):
+        instructor = session.query(Instructor).filter(instructor_id=data['instructor_id']).first()
+        instructor.instructor_name = data['instructor_name']
+        instructor.instructor_hours_required = data['instructor_hours_required']
+        instructor.instructor_notes = data['instructor_notes']
+        # ...and so on
+
+    def post(self, data):
+        inserted_instructor = Instructor(
+            instructor_name=data['instructor_name'],
+            instructor_hours_required=data['instructor_hours_required'],
+            instructor_notes=data['instructor_notes']
+        )
+        session.add(inserted_instructor)
+
+    def get(self, data):
+        x = session.query(Instructor).filter(Instructor.instructor_id == data['instructor_id']).first()
+        if x:
+            return x.to_data()
+        else:
+            return {"error": 'Hey, man, that\'s a bad burrito'}
+
+    def delete(self, data):
+        to_delete = session.query(Instructor).filter(instructor_id=data['instructor_id']).first()
+        if to_delete:
+            session.delete(to_delete)
+        else:
+            return {"error": 'Cannot delete; instructor does not exist.'}
 
 
-def post(data):
-    inserted_instructor = Instructor(
-        instructor_name=data['instructor_name'],
-        instructor_hours_required=data['instructor_hours_required'],
-        instructor_notes=data['instructor_notes']
-    )
-    session.add(inserted_instructor)
-
-
-def get(data):
-    x = session.query(Instructor).filter(Instructor.instructor_id == data['instructor_id']).first()
-    if x:
-        return x.to_data()
-    else:
-        return {"error": 'Hey, man, that\'s a bad burrito'}
-
-
-def delete(data):
-    to_delete = session.query(Instructor).filter(instructor_id=data['instructor_id']).first()
-    if to_delete:
-        session.delete(to_delete)
-    else:
-        return {"error": 'Cannot delete; instructor does not exist.'}
+    def on_get(self, req, resp, instructor_id):
+        resp.status = falcon.HTTP_200
+        resp.body = json.dumps(self.get({"instructor_id": instructor_id}))
--- data/route.py
+++ data/route.py
@@ -3,20 +3,11 @@ import json
 from data import instructor_controller
 from wsgiref import simple_server
 
-
-class InstructorController(object):
-    def on_get(self, req, resp, instructor_id):
-        resp.status = falcon.HTTP_200
-        resp.body = json.dumps(instructor_controller.get({"instructor_id": instructor_id}))
-
-
 app = falcon.API()
 
-instructor = InstructorController()
-
+instructor = instructor_controller.instructor_controller()
 
 app.add_route('/instructor/{instructor_id}', instructor)
-
 
 if __name__ == '__main__':
     httpd = simple_server.make_server('localhost', 8000, app)
