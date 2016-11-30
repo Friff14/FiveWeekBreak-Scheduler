33c5856973f62831b56eaec86f2a518b409e7e9f
Tanner Olsen <olsen.tanner.b@gmail.com>
2016-11-29 22:50:21 -0700

    Fixed bug with npm

---
+++ ngFWB/app/resources/campuses/campus-form.component.js
@@ -0,0 +1,39 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+/**
+ * Created by adsal on 11/26/2016.
+ */
+var core_1 = require('@angular/core');
+var campus_model_1 = require('./campus.model');
+var campus_service_1 = require("./campus.service");
+var CampusFormComponent = (function () {
+    function CampusFormComponent(campusService) {
+        this.campusService = campusService;
+        this.pageTitle = 'Add Campus';
+        this.model = new campus_model_1.Campus('Ogden', '123 Main St.');
+    }
+    CampusFormComponent.prototype.submitForm = function (form) {
+        console.log(this.model);
+        this.campusService.postCampusForm(this.model)
+            .subscribe(function (data) { return console.log('success: ', data); }, function (err) { return console.log('error: ', err); });
+    };
+    CampusFormComponent = __decorate([
+        core_1.Component({
+            selector: 'campus-form',
+            moduleId: module.id,
+            templateUrl: 'campus-form.component.html'
+        }), 
+        __metadata('design:paramtypes', [campus_service_1.CampusService])
+    ], CampusFormComponent);
+    return CampusFormComponent;
+}());
+exports.CampusFormComponent = CampusFormComponent;
+//# sourceMappingURL=campus-form.component.js.map
---
+++ ngFWB/app/resources/semesters/semester.module.js
@@ -0,0 +1,50 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+/**
+ * Created by Tanner_2 on 11/27/2016.
+ */
+var core_1 = require('@angular/core');
+var router_1 = require('@angular/router');
+var forms_1 = require('@angular/forms');
+var common_1 = require('@angular/common');
+var semester_list_component_1 = require('./semester-list.component');
+var semester_form_component_1 = require('./semester-form.component');
+var semester_service_1 = require('./semester.service');
+var SemesterModule = (function () {
+    function SemesterModule() {
+    }
+    SemesterModule = __decorate([
+        core_1.NgModule({
+            imports: [
+                common_1.CommonModule,
+                forms_1.FormsModule,
+                router_1.RouterModule.forChild([
+                    { path: 'semesterList', component: semester_list_component_1.SemesterListComponent },
+                    { path: 'semesterForm', component: semester_form_component_1.SemesterFormComponent }
+                ])
+            ],
+            declarations: [
+                semester_list_component_1.SemesterListComponent,
+                semester_form_component_1.SemesterFormComponent
+            ],
+            exports: [
+                semester_form_component_1.SemesterFormComponent
+            ],
+            providers: [
+                semester_service_1.SemesterService
+            ]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], SemesterModule);
+    return SemesterModule;
+}());
+exports.SemesterModule = SemesterModule;
+//# sourceMappingURL=semester.module.js.map
