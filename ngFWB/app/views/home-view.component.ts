c6ef546e980ce0cb1d028ee0aa9790435afec3db
adamsalvo <adsalvo95@gmail.com>
2016-12-07 08:42:12 -0700

    Export integrated

--- ngFWB/app/views/home-view.component.ts
+++ ngFWB/app/views/home-view.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {ISemester} from "../resources/semesters/semester";
+import {HomeService} from "./home.service";
+import {Semester} from "../resources/semesters/semester.model";
+import {Router} from "@angular/router";
 
 @Component({
     moduleId: module.id,
@@ -7,7 +11,25 @@ import { Component } from '@angular/core';
 })
 export class HomeViewComponent {
     pageTitle: string = 'Current Schedule';
+    semesters: ISemester[];
+    model = new Semester(null, null, null);
+    download_url = `http://localhost:8000/xlsx/1/output.xlsx`;
+     constructor(
+        private homeService: HomeService,
+        private router: Router) {
+    }
 
+    change_download_url(event): void{
+         this.download_url = `http://localhost:8000/xlsx/` + String(event) + '/output.xlsx'
+    }
+
+    ngOnInit(): void {
+        console.log(this.homeService);
+        this.homeService.getSemesters()
+            .subscribe(semesters => this.semesters = semesters,
+                error => console.log('get error: ', error));
+
+    }
     /*
     downloadURI(uri, name) {
         var link = document.createElement("a");
