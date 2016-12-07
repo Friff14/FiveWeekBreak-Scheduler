c9cc9a807cccbdc1f339617ed745ff92f0ab44c8
eugkimStar <eugenekim@mail.weber.edu>
2016-12-07 09:02:34 -0700

    Added handleError

--- ngFWB/app/views/home.service.ts
+++ ngFWB/app/views/home.service.ts
@@ -25,5 +25,17 @@ export class HomeService {
         .catch(this.handleError);
     }
 
-
+    private handleError (error: Response | any) {
+        let errMsg: string;
+        if (error instanceof Response) {
+            const body = error.json() || '';
+            const err = body.error || JSON.stringify(body);
+            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+        }
+        else {
+            errMsg = error.message ? error.message : error.toString(); 
+        }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
+    }
 }
