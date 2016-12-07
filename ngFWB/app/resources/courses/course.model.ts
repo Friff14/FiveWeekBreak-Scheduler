c33677d55097c397e3adf7bcb995c789bd15e9d6
Tanner Olsen <olsen.tanner.b@gmail.com>
2016-12-06 22:05:48 -0700

    Made the course do stuffs

--- ngFWB/app/resources/courses/course.model.ts
+++ ngFWB/app/resources/courses/course.model.ts
@@ -2,10 +2,11 @@ import { IPrefix } from '../prefixes/prefix';
 
 export class Course {//extends IResource {
     constructor(
-    // public course_id: number,
-    public course_name: string,
-    public course_description: string,
-    public course_credit_hours: number,
-    public prefix: IPrefix
+        public course_id: number,
+        public course_name: string,
+        public course_number: string,
+        public course_description: string,
+        public course_credit_hours: number,
+        public prefix_id: number
     ){}
 }
--- ngFWB/app/resources/courses/course.ts
+++ ngFWB/app/resources/courses/course.ts
@@ -3,8 +3,9 @@ import { IPrefix } from '../prefixes/prefix';
 export interface ICourse {//extends IResource {
     course_id: number;
     course_name: string;
+    course_number: string;
     course_description: string;
-    course_credit_hours: string;
+    course_credit_hours: number;
     prefix: IPrefix;
 }
 
